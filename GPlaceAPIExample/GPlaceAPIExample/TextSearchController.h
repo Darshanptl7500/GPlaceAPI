4b504c2c46998d31bf82c34a979eb933afcf3e8b
Darshan Patel <darshanptl89@gmail.com>
2015-07-21 18:37:56 +0530

    small change

--- GPlaceAPIExample/GPlaceAPIExample/TextSearchController.h
+++ GPlaceAPIExample/GPlaceAPIExample/TextSearchController.h
@@ -2,7 +2,7 @@
 //  TextSearchController.h
 //  GPlaceAPIExample
 //
-//  Created by IQR IT on 7/21/15.
+//  Created by Darshan Patel on 7/21/15.
 //  Copyright (c) 2015 Darshan Patel. All rights reserved.
 //
 
--- GPlaceAPIExample/GPlaceAPIExample/TextSearchController.m
+++ GPlaceAPIExample/GPlaceAPIExample/TextSearchController.m
@@ -2,7 +2,7 @@
 //  TextSearchController.m
 //  GPlaceAPIExample
 //
-//  Created by IQR IT on 7/21/15.
+//  Created by Darshan Patel on 7/21/15.
 //  Copyright (c) 2015 Darshan Patel. All rights reserved.
 //
 
